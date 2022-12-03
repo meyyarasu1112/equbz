372d26bf353d02804d5d70f0b885a2df72357817
meyyarasu1112 <68989037+meyyarasu1112@users.noreply.github.com>
2022-12-03 12:35:50 +0530

    Add files via upload

---
+++ components/Topbar.jsx
@@ -0,0 +1,127 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+// import AdbIcon from '@mui/icons-material/Adb';
+import logo from "../images/Rectangle logo.jpg";
+
+const pages = ["Home","Products", "Pricing", "Contact"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+function Topbar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    console.log(event.currentTarget);
+    setAnchorElNav(event.currentTarget);
+  };  
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+ 
+
+  return (
+    <AppBar position="fixed" color="default">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{display:"flex",alignItems:"center"}}>
+            {/* <Avatar src={logo} sx={{ width: 56, height: 56 }}/>            
+            <Typography
+              variant="h4"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                ml: 2,
+                display: { xs: "flex", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                // letterSpacing: '.3rem',
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              <span style={{color:"#82CD47"}}>e</span>Qubz
+            </Typography> */}
+            <img src={logo} width={100} height={50} />
+          </Box>
+
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "flex", sm: "none" },
+              justifyContent: "end",
+            }}
+          >
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", sm: "flex" },
+              justifyContent: "end",
+            }}
+          >
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "black", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>          
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Topbar;
